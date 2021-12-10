75807414d6c2a7a5de654ee8d83b3c916e920d25
Afired <conor.huismann@gmail.com>
2021-12-10 23:45:53 +0100

    basic initialization and update loop on separate thread

--- GameEngine/Core/Game.cs
+++ GameEngine/Core/Game.cs
@@ -1,5 +1,28 @@
-﻿namespace GameEngine.Core;
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace GameEngine.Core;
+
+public delegate void OnUpdate(float deltaTime);
 
 public class Game {
-
+    
+    public static event OnUpdate OnUpdate;
+    public bool IsRunning;
+    
+    
+    public void Initialize() {
+        IsRunning = true;
+        new Thread(new ThreadStart(UpdateLoop)).Start();
+    }
+    
+    private void UpdateLoop() {
+        Stopwatch stopwatch = new();
+        while(IsRunning) {
+            float deltaTime = stopwatch.Elapsed.Seconds;
+            stopwatch.Restart();
+            OnUpdate?.Invoke(deltaTime);
+        }
+    }
+    
 }

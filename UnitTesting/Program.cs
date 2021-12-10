f9d7630b8171102458b8fa9b07c60fa4046b0671
Afired <conor.huismann@gmail.com>
2021-12-11 00:41:27 +0100

    added test for games update loop

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace UnitTesting;
+﻿using System;
+using System.Threading;
+using GameEngine.Core;
+using NUnit.Framework;
+
+namespace UnitTesting;
 
 internal static class Program {
-    
+
     public static int Main(string[] args) {
         return 0;
     }
     
+    [Test]
+    public static void TestUpdateLoop() {
+        float testForSeconds = 5f;
+        
+        Game game = new();
+        game.Initialize();
+
+        int updateLoopCalled = 0;
+        
+        Game.OnUpdate += deltaTime => {
+            updateLoopCalled++;
+        };
+        
+        Thread.Sleep(TimeSpan.FromSeconds(testForSeconds));
+        Console.WriteLine($"UpdateLoop called {updateLoopCalled} in {testForSeconds} seconds");
+        Assert.Greater(updateLoopCalled, 10);
+    }
+
 }

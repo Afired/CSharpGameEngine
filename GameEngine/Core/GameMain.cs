de10be6b916f4c3d32ec78a8c0388513e5e4f20e
Afired <conor.huismann@gmail.com>
2021-12-11 19:40:04 +0100

    refactored game class into separate partial classes to differentiate between threads

---
+++ GameEngine/Core/GameMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using GameEngine.Rendering;
+using GLFW;
+using OpenGL;
+
+namespace GameEngine.Core;
+
+public sealed partial class Game {
+    
+    private bool _isRunning;
+    private Thread _updateLoopThread;
+    private Thread _physicsThread;
+    private Thread _renderThread;
+    
+    
+    public void Initialize() {
+        _isRunning = true;
+        _updateLoopThread = new Thread(UpdateLoop);
+        _physicsThread = new Thread(FixedUpdateLoop);
+        _renderThread = new Thread(StartRenderThread);
+    }
+    
+    public void Start() {
+        _updateLoopThread.Start();
+        _physicsThread.Start();
+        _renderThread.Start();
+    }
+    
+    private void Terminate() {
+        _isRunning = false;
+    }
+    
+}
---
+++ GameEngine/Core/GameRenderThread.cs
@@ -0,0 +1,26 @@
+﻿using GameEngine.Rendering;
+using GLFW;
+using OpenGL;
+
+namespace GameEngine.Core; 
+
+public sealed partial class Game {
+    
+    private void StartRenderThread() {
+        Window window = WindowFactory.CreateWindow(900, 600, "Window Title", false);
+
+        while(!Glfw.WindowShouldClose(window)) {
+            Glfw.PollEvents();
+            Render(window);
+        }
+        Terminate();
+    }
+    
+    private void Render(Window window) {
+        GL.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT);
+        
+        Glfw.SwapBuffers(window);
+    }
+    
+}

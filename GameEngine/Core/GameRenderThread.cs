b2dc90a5aa28fb40be548c7565ac5525ab7a6c67
Afired <conor.huismann@gmail.com>
2021-12-11 23:16:39 +0100

    compile GLSL shader and display geometry with vertex data

--- GameEngine/Core/GameRenderThread.cs
+++ GameEngine/Core/GameRenderThread.cs
@@ -1,4 +1,5 @@
 ﻿using GameEngine.Rendering;
+using GameEngine.Rendering.Shaders;
 using GLFW;
 using OpenGL;
 
@@ -8,6 +9,8 @@ public sealed partial class Game {
     
     private void StartRenderThread() {
         Window window = WindowFactory.CreateWindow(900, 600, "Window Title", false);
+        
+        Load();
 
         while(!Glfw.WindowShouldClose(window)) {
             Glfw.PollEvents();
@@ -16,9 +19,76 @@ public sealed partial class Game {
         Terminate();
     }
     
+    private uint _vao;
+    private uint _vbo;
+    private Shader _shader;
+    
+    private const string VERTEX_SHADER = @"#version 330 core
+                                    layout (location = 0) in vec2 aPosition;
+                                    layout (location = 1) in vec3 aColor;
+                                    out vec4 vertexColor;
+    
+                                    void main() 
+                                    {
+                                        vertexColor = vec4(aColor.rgb, 1.0);
+                                        gl_Position = vec4(aPosition.xy, 0, 1.0);
+                                    }";
+
+    private const string FRAGMENT_SHADER = @"#version 330 core
+                                    out vec4 FragColor;
+                                    in vec4 vertexColor;
+
+                                    void main() 
+                                    {
+                                        FragColor = vertexColor;
+                                    }";
+
+    private void Load() {
+        _shader = new Shader(VERTEX_SHADER, FRAGMENT_SHADER);
+        _shader.Load();
+
+        _vao = GL.glGenVertexArray();
+        _vbo = GL.glGenBuffer();
+        
+        GL.glBindVertexArray(_vao);
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, _vbo);
+        
+        float[] vertices =
+        {
+            -0.5f, 0.5f, 1f, 0f, 0f,  // top left
+            0.5f, 0.5f, 0f, 1f, 0f,   // top right
+            -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+
+            0.5f, 0.5f, 0f, 1f, 0f,   // top right
+            0.5f, -0.5f, 0f, 1f, 1f,  // bottom right
+            -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+        };
+
+        unsafe {
+            fixed(float* v = &vertices[0]) {
+                GL.glBufferData(GL.GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, v, GL.GL_STATIC_DRAW);
+            }
+            
+            GL.glVertexAttribPointer(0, 2, GL.GL_FLOAT, false, 5 * sizeof(float), (void*) (0 * sizeof(float)));
+            GL.glEnableVertexAttribArray(0);
+            
+            GL.glVertexAttribPointer(1, 3, GL.GL_FLOAT, false, 5 * sizeof(float), (void*) (2 * sizeof(float)));
+            GL.glEnableVertexAttribArray(1);
+            
+            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0);
+            GL.glBindVertexArray(0);
+        }
+    }
+
     private void Render(Window window) {
-        GL.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        GL.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         GL.glClear(GL.GL_COLOR_BUFFER_BIT);
+        
+        _shader.Use();
+        
+        GL.glBindVertexArray(_vao);
+        GL.glDrawArrays(GL.GL_TRIANGLES, 0, 6);
+        GL.glBindVertexArray(0);
         
         Glfw.SwapBuffers(window);
     }
---
+++ GameEngine/Rendering/Shaders/Shader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenGL;
+
+namespace GameEngine.Rendering.Shaders; 
+
+public class Shader {
+    
+    public uint ProgramID { get; set; }
+    private string _vertexCode;
+    private string _fragmentCode;
+
+
+    public Shader(string vertexCode, string fragmentCode) {
+        _vertexCode = vertexCode;
+        _fragmentCode = fragmentCode;
+    }
+
+    public void Load() {
+        uint vs = GL.glCreateShader(GL.GL_VERTEX_SHADER);
+        GL.glShaderSource(vs, _vertexCode);
+        GL.glCompileShader(vs);
+
+        int[] status = GL.glGetShaderiv(vs, GL.GL_COMPILE_STATUS, 1);
+        if(status[0] == 0) {
+            string error = GL.glGetShaderInfoLog(vs);
+            Console.WriteLine($"FAILED TO COMPILE VERTEX SHADER: {error}");
+        }
+        
+        uint fs = GL.glCreateShader(GL.GL_FRAGMENT_SHADER);
+        GL.glShaderSource(fs, _fragmentCode);
+        GL.glCompileShader(fs);
+        
+        status = GL.glGetShaderiv(fs, GL.GL_COMPILE_STATUS, 1);
+        if(status[0] == 0) {
+            string error = GL.glGetShaderInfoLog(fs);
+            Console.WriteLine($"FAILED TO COMPILE FRAGMENT SHADER: {error}");
+        }
+
+        ProgramID = GL.glCreateProgram();
+        GL.glAttachShader(ProgramID, vs);
+        GL.glAttachShader(ProgramID, fs);
+        
+        GL.glLinkProgram(ProgramID);
+        
+        // Delete Shaders
+        
+        GL.glDetachShader(ProgramID, vs);
+        GL.glDetachShader(ProgramID, fs);
+        GL.glDeleteShader(vs);
+        GL.glDeleteShader(fs);
+    }
+
+    public void Use() {
+        GL.glUseProgram(ProgramID);
+    }
+    
+}

5ccbb829f8769972a2776c1271b69cae6f9abd47
Afired <conor.huismann@gmail.com>
2021-12-11 01:11:46 +0100

    added fixed update loop and callback

--- GameEngine/Core/Game.cs
+++ GameEngine/Core/Game.cs
@@ -1,19 +1,23 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace GameEngine.Core;
 
 public delegate void OnUpdate(float deltaTime);
+public delegate void OnFixedUpdate(float fixedDeltaTime);
 
 public class Game {
     
     public static event OnUpdate OnUpdate;
+    public static event OnFixedUpdate OnFixedUpdate;
     public bool IsRunning;
     
     
     public void Initialize() {
         IsRunning = true;
         new Thread(new ThreadStart(UpdateLoop)).Start();
+        new Thread(new ThreadStart(FixedUpdateLoop)).Start();
     }
     
     private void UpdateLoop() {
@@ -22,6 +26,17 @@ public class Game {
             float deltaTime = (float) stopwatch.Elapsed.TotalSeconds;
             stopwatch.Restart();
             OnUpdate?.Invoke(deltaTime);
+        }
+    }
+    
+    private void FixedUpdateLoop() {
+        Stopwatch stopwatch = new();
+        while(IsRunning) {
+            float elapsedTime = (float) stopwatch.Elapsed.TotalSeconds;
+            TimeSpan timeOut = TimeSpan.FromSeconds(Configuration.FixedTimeStep - elapsedTime);
+            Thread.Sleep(timeOut);
+            stopwatch.Restart();
+            OnFixedUpdate?.Invoke(Configuration.FixedTimeStep);
         }
     }
     

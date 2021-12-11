73c0ca9eb63aa662ac17bf33841b5528072738d3
Afired <conor.huismann@gmail.com>
2021-12-11 01:12:07 +0100

    added fixed update loop test

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -28,5 +28,23 @@ internal static class Program {
         Console.WriteLine($"UpdateLoop called {updateLoopCalled} in {testForSeconds} seconds");
         Assert.Greater(updateLoopCalled, 10);
     }
+    
+    [Test]
+    public static void TestFixedUpdateLoop() {
+        float testForSeconds = 5f;
+        
+        Game game = new();
+        game.Initialize();
+
+        int fixedUpdateLoopCalled = 0;
+        
+        Game.OnFixedUpdate += fixedDeltaTime => {
+            fixedUpdateLoopCalled++;
+        };
+        
+        Thread.Sleep(TimeSpan.FromSeconds(testForSeconds));
+        Console.WriteLine($"FixedUpdateLoop called {fixedUpdateLoopCalled} in {testForSeconds} seconds");
+        Assert.Greater(fixedUpdateLoopCalled, 0);
+    }
 
 }

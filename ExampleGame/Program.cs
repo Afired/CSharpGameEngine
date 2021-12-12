9be881d9899a7404a01c17549d0f73190715d78f
Afired <conor.huismann@gmail.com>
2021-12-12 04:01:42 +0100

    refactored example game

--- ExampleGame/Program.cs
+++ ExampleGame/Program.cs
@@ -10,21 +10,9 @@ internal class Program {
         Game game = new Game();
         
         game.Initialize();
-
-        Camera2D camera = new Camera2D(10.0f);
-        camera.Transform.Position.X = 0;
-        Game.SetActiveCamera(camera);
-
-        Plane plane = new Plane();
         
+        InitializeWorld();
         
-        Game.OnUpdate += deltaTime => {
-            //Game.CurrentCamera.Transform.Position.X += deltaTime * 1;
-            //Console.WriteLine(camera.Transform.Position.X);
-            //Game.CurrentCamera.Zoom += deltaTime * 5.0f;
-            Game.CurrentCamera.Transform.Position.X += deltaTime;
-        };
-
         game.Start();
         
         return 0;
@@ -33,18 +21,12 @@ internal class Program {
     private static void InitializeWorld() {
         Camera2D camera = new Camera2D(10.0f);
         camera.Transform.Position.X = 0;
+        Game.OnUpdate += deltaTime => camera.Transform.Position.X += deltaTime;
         Game.SetActiveCamera(camera);
 
-        Plane plane = new Plane();
-        
-        
-        Game.OnUpdate += deltaTime => {
-            //Game.CurrentCamera.Transform.Position.X += deltaTime * 1;
-            //Console.WriteLine(camera.Transform.Position.X);
-            //Game.CurrentCamera.Zoom += deltaTime * 5.0f;
-            Game.CurrentCamera.Transform.Position.X += deltaTime;
-        };
-        
+        Plane plane1 = new Plane();
+        Game.OnUpdate += deltaTime => plane1.Transform.Position.X += deltaTime;
+        Game.OnUpdate += deltaTime => plane1.Transform.Position.Y += deltaTime;
     }
     
 }

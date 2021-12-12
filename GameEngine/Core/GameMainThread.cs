4a5098ce6cfc6a5ac78974c07cc63955cce06f36
Afired <conor.huismann@gmail.com>
2021-12-12 12:02:24 +0100

    added initialization and starting logs

--- GameEngine/Core/GameMainThread.cs
+++ GameEngine/Core/GameMainThread.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using GameEngine.Debugging;
 using GameEngine.Rendering.Cameras;
 
 namespace GameEngine.Core;
@@ -13,16 +14,32 @@ public sealed partial class Game {
     
     
     public void Initialize() {
+        Console.Log("Initializing...");
+        Console.Log("Initializing engine...");
         _updateLoopThread = new Thread(UpdateLoop);
+        Console.LogSuccess("Initialized engine (1/3)");
+        Console.Log("Initializing physics engine...");
         _physicsThread = new Thread(FixedUpdateLoop);
+        Console.LogSuccess("Initialized physics engine (2/3)");
+        Console.Log("Initializing render engine...");
         _renderThread = new Thread(StartRenderThread);
+        Console.LogSuccess("Initialized render engine (3/3)");
+        Console.LogSuccess("Initialization complete");
     }
     
     public void Start() {
         _isRunning = true;
+        Console.Log("Starting...");
+        Console.Log("Starting engine...");
         _updateLoopThread.Start();
+        Console.LogSuccess("Started engine (1/3)");
+        Console.Log("Starting physics engine...");
         _physicsThread.Start();
+        Console.LogSuccess("Started physics engine (2/3)");
+        Console.Log("Initializing render engine...");
         _renderThread.Start();
+        Console.LogSuccess("Initialized render engine (3/3)");
+        Console.LogSuccess("Started");
     }
 
     public static void SetActiveCamera(Camera2D camera2D) {
@@ -31,6 +48,7 @@ public sealed partial class Game {
     
     private void Terminate() {
         _isRunning = false;
+        Console.Log("Terminating...");
     }
     
 }

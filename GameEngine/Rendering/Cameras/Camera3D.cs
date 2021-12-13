088b482cef140d4f860370515fd00d6dfdd420fb
Afired <59524587+Afired@users.noreply.github.com>
2021-12-13 15:46:19 +0100

    added camera rotation to projection matrix

--- GameEngine/Rendering/Cameras/Camera3D.cs
+++ GameEngine/Rendering/Cameras/Camera3D.cs
@@ -9,10 +9,10 @@ public class Camera3D : BaseCamera {
     
     public override Matrix4x4 GetProjectionMatrix() {
         Matrix4x4 transMatrix = Matrix4x4.CreateTranslation(Transform.Position);
-        //Matrix4x4 rot = Matrix4x4.CreateFromYawPitchRoll(Transform.Rotation.X, Transform.Rotation.Y, Transform.Rotation.Z);
+        Matrix4x4 rot = Matrix4x4.CreateFromYawPitchRoll(Transform.Rotation.X, Transform.Rotation.Y, Transform.Rotation.Z);
         Matrix4x4 perMatrix = Matrix4x4.CreatePerspectiveFieldOfView(2f, (float) Configuration.WindowWidth / (float) Configuration.WindowHeight, 0.01f, 100f);
-
-        return transMatrix /** rot*/ * perMatrix;
+        
+        return transMatrix * rot * perMatrix;
     }
     
 }

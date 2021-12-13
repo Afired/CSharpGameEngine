8a37be4d21177a033eee38c644b0c1261925d97a
Afired <59524587+Afired@users.noreply.github.com>
2021-12-13 09:26:14 +0100

    changed formatting of default shader

--- GameEngine/Rendering/Shaders/DefaultShader.cs
+++ GameEngine/Rendering/Shaders/DefaultShader.cs
@@ -2,28 +2,36 @@
 
 internal static class DefaultShader {
     
-    private const string VERTEX_SHADER = @"#version 330 core
-                                    layout (location = 0) in vec2 aPosition;
-                                    layout (location = 1) in vec3 aColor;
-                                    out vec4 vertexColor;
-                                    
-                                    uniform mat4 projection;
-                                    uniform mat4 model;
-                                    
-                                    void main() 
-                                    {
-                                        vertexColor = vec4(aColor.rgb, 1.0);
-                                        gl_Position = projection * model * vec4(aPosition.xy, 0, 1.0);
-                                    }";
+    private const string VERTEX_SHADER = @"
 
-    private const string FRAGMENT_SHADER = @"#version 330 core
-                                    out vec4 FragColor;
-                                    in vec4 vertexColor;
-                                    
-                                    void main() 
-                                    {
-                                        FragColor = vertexColor;
-                                    }";
+#version 330 core
+layout (location = 0) in vec2 aPosition;
+layout (location = 1) in vec3 aColor;
+out vec4 vertexColor;
+
+uniform mat4 projection;
+uniform mat4 model;
+
+void main() 
+{
+    vertexColor = vec4(aColor.rgb, 1.0);
+    gl_Position = projection * model * vec4(aPosition.xy, 0, 1.0);
+}
+
+";
+
+    private const string FRAGMENT_SHADER = @"
+
+#version 330 core
+out vec4 FragColor;
+in vec4 vertexColor;
+
+void main() 
+{
+    FragColor = vertexColor;
+}
+
+";
 
     internal static void Initialize() {
         Shader shader = new Shader(VERTEX_SHADER, FRAGMENT_SHADER);

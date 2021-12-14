274eeb5edcaa2a163769dfc082234801a49b8aea
Afired <59524587+Afired@users.noreply.github.com>
2021-12-14 14:58:51 +0100

    clean up

--- GameEngine/Numerics/RotatePointWithQuaternion.cs
+++ GameEngine/Numerics/RotatePointWithQuaternion.cs
@@ -9,11 +9,3 @@ public partial struct Quaternion : IEquatable<Quaternion> {
     }
     
 }
-
-/*
-Actually to rotate a vector / point by a quaternion "q" you have to do
-
-p' = q * p * q^-1
-
-where q^-1 is the complex conjugate of q. It has to be done is this order. Unity does this "sandwich multiplication" internally for you when you do q * p. I recommend to watch this 3blue1brown video on quaternions.
-*/

9b2ece312bdd34cebfcf78ad3042d2f22168fb7f
Afired <conor.huismann@gmail.com>
2021-12-23 17:37:55 +0100

    refactored frame buffer into its own class

--- GameEngine/Core/GameRenderThread.cs
+++ GameEngine/Core/GameRenderThread.cs
@@ -19,7 +19,7 @@ public sealed partial class Game {
     
     private void StartRenderThread() {
         // initial setup
-        Setup(out Window window, out uint framebuffer, out uint textureColorBuffer, out uint vao);
+        Setup(out Window window, out FrameBuffer frameBuffer, out uint vao);
         InputHandler inputHandler = new InputHandler();
         Glfw.SetKeyCallback(window, inputHandler.OnKeyAction);
         
@@ -28,7 +28,7 @@ public sealed partial class Game {
             
             // render and draw frame
             if(CurrentCamera != null)
-                Render(window, framebuffer, textureColorBuffer, vao);
+                Render(window, frameBuffer, vao);
             
             // handle input
             Glfw.PollEvents();
@@ -39,10 +39,10 @@ public sealed partial class Game {
         Terminate();
     }
 
-    private void Setup(out Window window, out uint framebuffer, out uint textureColorBuffer, out uint vao) {
+    private void Setup(out Window window, out FrameBuffer frameBuffer, out uint vao) {
         window = WindowFactory.CreateWindow();
-
-        SetupFrameBuffers(out framebuffer, out textureColorBuffer);
+        
+        frameBuffer = new FrameBuffer();
         
         vao = GetFullScreenRenderQuadVao();
         
@@ -55,15 +55,15 @@ public sealed partial class Game {
         OnLoad?.Invoke();
     }
 
-    private void Render(Window window, uint framebuffer, uint textureColorBuffer, uint vao) {
-        RenderFirstPass(framebuffer);
-        RenderSecondPass(textureColorBuffer, vao);
+    private void Render(Window window, FrameBuffer frameBuffer, uint vao) {
+        RenderFirstPass(frameBuffer.ID);
+        RenderSecondPass(frameBuffer.TextureColorBuffer, vao);
         Glfw.SwapBuffers(window);
     }
 
-    private void RenderFirstPass(uint framebuffer) {
+    private void RenderFirstPass(uint frameBuffer) {
         // bind custom framebuffer to render to
-        GL.glBindFramebuffer(framebuffer);
+        GL.glBindFramebuffer(frameBuffer);
         GL.glClear(GL.GL_DEPTH_BUFFER_BIT | GL.GL_COLOR_BUFFER_BIT);
         GL.glEnable(GL.GL_DEPTH_TEST); // reenable depth test
         OnDraw?.Invoke();
@@ -87,52 +87,6 @@ public sealed partial class Game {
 
     private void RenderBackground() {
         GL.glClearColor(CurrentCamera.BackgroundColor.R, CurrentCamera.BackgroundColor.G, CurrentCamera.BackgroundColor.B, CurrentCamera.BackgroundColor.A);
-    }
-
-    private void SetupFrameBuffers(out uint framebuffer, out uint textureColorBuffer) {
-        // create and bind frame buffer object
-        framebuffer = GL.glGenFramebuffer();
-        GL.glBindFramebuffer(GL.GL_FRAMEBUFFER, framebuffer);
-
-        // delete framebuffer when all framebuffer operations are done
-        //GL.glDeleteBuffer(framebuffer);
-        
-        // attach textures buffer object
-        textureColorBuffer = GL.glGenTexture();
-        GL.glBindTexture(GL.GL_TEXTURE_2D, textureColorBuffer);
-        unsafe {
-            if(Configuration.UseHDR)
-                GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, (int) InternalFormat.Rgb16f, Configuration.WindowWidth, Configuration.WindowHeight, 0, (int) PixelFormat.Rgb, GL.GL_FLOAT, null);
-            else
-                GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, (int) InternalFormat.Rgb, Configuration.WindowWidth, Configuration.WindowHeight, 0, (int) PixelFormat.Rgb, GL.GL_UNSIGNED_BYTE, null);
-        }
-        GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR);
-        GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
-        GL.glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_COLOR_ATTACHMENT0, GL.GL_TEXTURE_2D, textureColorBuffer, 0);
-        
-        // attach depth stencil buffer
-        //unsafe {
-        //    GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_DEPTH24_STENCIL8, 800, 600, 0, GL.GL_DEPTH_STENCIL, GL.GL_UNSIGNED_INT_24_8, null);
-        //}
-        //GL.glFramebufferTexture2D(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_STENCIL_ATTACHMENT, GL.GL_TEXTURE_2D, texture, 0); 
-        
-        // attach render buffer object
-        uint rbo = GL.glGenRenderbuffer();
-        GL.glBindRenderbuffer(rbo);
-        GL.glRenderbufferStorage(GL.GL_RENDERBUFFER, GL.GL_DEPTH24_STENCIL8, Configuration.WindowWidth, Configuration.WindowHeight);
-        
-        GL.glBindRenderbuffer(0);
-
-        GL.glFramebufferRenderbuffer(GL.GL_FRAMEBUFFER, GL.GL_DEPTH_STENCIL_ATTACHMENT, GL.GL_RENDERBUFFER, rbo);
-        
-        //check for framebuffer status
-        if(GL.glCheckFramebufferStatus(GL.GL_FRAMEBUFFER) == GL.GL_FRAMEBUFFER_COMPLETE)
-            Console.LogSuccess("Framebuffer setup succeeded");
-        else
-            Console.LogError("Framebuffer is not complete");
-        
-        // bind default frame buffer to render to
-        GL.glBindFramebuffer(GL.GL_FRAMEBUFFER, 0);
     }
 
     private uint GetFullScreenRenderQuadVao() {

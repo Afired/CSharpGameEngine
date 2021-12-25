e8f767be2368c8c745c52604bdb05398d4153639
Afired <conor.huismann@gmail.com>
2021-12-25 16:04:23 +0100

    make direct glfw calls

--- ImGUITesting/Program.cs
+++ ImGUITesting/Program.cs
@@ -3,20 +3,51 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
-using System.Drawing;
+using Silk.NET.GLFW;
 using Silk.NET.Windowing;
 using Silk.NET.Input;
+using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing.Glfw;
+using VideoMode = Silk.NET.Windowing.VideoMode;
 
 namespace ImGui
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            // Create a Silk.NET window as usual
-            using var window = Window.Create(WindowOptions.Default);
+            
+            WindowOptions windowOptions = new WindowOptions() {
+                Position = new Vector2D<int>(-1, -1), // ? doesnt work
+                Samples = 1, // multisample anti aliasing?
+                Size = new Vector2D<int>(1600, 900), // size of the window in pixel
+                Title = "Title", // title of the window
+                IsVisible = true, // ?
+                TransparentFramebuffer = false, // makes window transparent as long as no color is drawn
+                VideoMode = VideoMode.Default,
+                VSync = true, // vertical synchronisation
+                WindowBorder = WindowBorder.Fixed, // window border type
+                WindowClass = "idk", // ?
+                WindowState = WindowState.Normal, // window state
+                API = new GraphicsAPI(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Debug, new APIVersion(3, 3)), // graphics api
+                FramesPerSecond = -1, // fps
+                IsEventDriven = true, // ?
+                PreferredBitDepth = new Vector4D<int>(255, 255, 255, 255), // ?
+                ShouldSwapAutomatically = false, // if true swaps frame buffers at the end of rendering automatically
+                UpdatesPerSecond = 1, // ? polling?
+                IsContextControlDisabled = true, // ?
+                PreferredDepthBufferBits = 255, // ?
+                PreferredStencilBufferBits = 255, // ?
+            };
+            
+            
+            GlfwWindowing.Use();
+
+            // Create Silk.NET window with window options
+            /*using*/ var window = Window.Create(windowOptions);
 
             // Declare some variables
             ImGuiController controller = null;
@@ -24,13 +55,16 @@ namespace ImGui
             IInputContext inputContext = null;
 
             // Our loading function
-            window.Load += () =>
-            {
-                controller = new ImGuiController(
-                    gl = window.CreateOpenGL(), // load OpenGL
-                    window, // pass in our window
-                    inputContext = window.CreateInput() // create an input context
-                );
+            window.Load += () => {
+                controller = new ImGuiController(gl = window.CreateOpenGL(), window, inputContext = window.CreateInput());
+                Console.WriteLine(GlfwWindowing.IsViewGlfw(window));
+                Glfw glfw = Glfw.GetApi();
+                glfw.Init();
+                unsafe {
+                    //glfw.HideWindow(GlfwWindowing.GetHandle(window));
+                    Cursor* cursor = glfw.CreateStandardCursor(CursorShape.Crosshair);
+                    glfw.SetCursor(GlfwWindowing.GetHandle(window), cursor);
+                }
             };
 
             // Handle resizes
@@ -48,7 +82,7 @@ namespace ImGui
 
                 // This is where you'll do any rendering beneath the ImGui context
                 // Here, we just have a blank screen.
-                gl.ClearColor(Color.FromArgb(255, (int) (.45f * 255), (int) (.55f * 255), (int) (.60f * 255)));
+               // gl.ClearColor(Color.FromArgb(255, (int) (.45f * 255), (int) (.55f * 255), (int) (.60f * 255)));
                 gl.Clear((uint) ClearBufferMask.ColorBufferBit);
 
                 // This is where you'll do all of your ImGUi rendering
@@ -57,6 +91,7 @@ namespace ImGui
 
                 // Make sure ImGui renders too!
                 controller.Render();
+                window.SwapBuffers();
             };
 
             // The closing function

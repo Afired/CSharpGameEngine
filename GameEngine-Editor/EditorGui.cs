14294506e6a4cf6e0a1b35ec9e76292c55caf9ef
Afired <conor.huismann@gmail.com>
2021-12-27 21:54:03 +0100

    basic editor setup

---
+++ GameEngine-Editor/EditorGui.cs
@@ -0,0 +1,20 @@
+using ImGuiNET;
+
+namespace GameEngine.Editor; 
+
+public class EditorGui {
+
+    public EditorGui() {
+        Initialize();
+    }
+
+    private void Initialize() {
+        OnImGui += OnImGuiDraw;
+    }
+
+    private void OnImGuiDraw() {
+        ImGui.DockSpaceOverViewport();
+        ImGui.ShowDemoWindow();
+    }
+    
+}
--- GameEngine-Editor/Program.cs
+++ GameEngine-Editor/Program.cs
@@ -1,8 +1,20 @@
-﻿namespace GameEngine.Editor;
+﻿using GameEngine.Core;
+
+namespace GameEngine.Editor;
 
 public static class Program {
 
     public static int Main(string[] args) {
+
+        Application application = new Application();
+        
+        application.Initialize();
+
+        Configuration.WindowTitle = "GameEngine-Editor";
+        
+        EditorGui editorGui = new EditorGui();
+        
+        application.Start();
         
         return 0;
     }

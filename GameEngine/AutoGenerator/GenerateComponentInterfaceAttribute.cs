20bd1d8844d22e192e66f87034c9a16443665844
Afired <conor.huismann@gmail.com>
2022-01-07 04:02:48 +0100

    added more attributes for auto gen

--- GameEngine/AutoGenerator/GenerateComponentInterfaceAttribute.cs
+++ GameEngine/AutoGenerator/GenerateComponentInterfaceAttribute.cs
@@ -4,3 +4,23 @@ namespace GameEngine.AutoGenerator;
 
 [AttributeUsage(AttributeTargets.Class)]
 public class GenerateComponentInterfaceAttribute : Attribute { }
+
+//todo: generate Component interfaces as default if the class derives from component and is not abstract
+
+//todo: waiting for c#11 for generic attributes
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class RequireComponent : Attribute {
+    
+    public RequireComponent(Type component) {
+        
+    }
+    
+    public RequireComponent(params Type[] components) {
+        
+    }
+    
+}
+
+//todo: if you want to manually want to create the component interface, you can use this attribute
+[AttributeUsage(AttributeTargets.Class)]
+public class DoNotGenerateComponent : Attribute { }

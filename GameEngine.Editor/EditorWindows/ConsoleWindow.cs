115b188bf08fcbebb132edc3f00c230dc07ae919
Conor <conor.huismann@gmail.com>
2022-05-06 01:52:58 +0200

    updated LogMessage to be Record

--- GameEngine.Editor/EditorWindows/ConsoleWindow.cs
+++ GameEngine.Editor/EditorWindows/ConsoleWindow.cs
@@ -12,8 +12,8 @@ public class ConsoleWindow : EditorWindow {
     private bool _showSuccessLogMessages = true;
     private bool _showWarningLogMessages = true;
     private bool _showErrorLogMessages = true;
-
-
+    
+    
     public ConsoleWindow() {
         Title = "Console";
         _logMessages = new List<LogMessage>();
@@ -31,7 +31,7 @@ public class ConsoleWindow : EditorWindow {
         DrawToolBar();
         DrawLogs();
     }
-
+    
     private void DrawLogs() {
         for(int i = 0; i < _logMessages.Count; i++) {
 //            if(_logMessages[i].LogSeverity == LogSeverity.Error && !_showErrorLogMessages)
@@ -39,27 +39,22 @@ public class ConsoleWindow : EditorWindow {
             _logMessages[i].Draw();
         }
     }
-
+    
     private void DrawToolBar() {
         if(ImGui.Button("Clear"))
             _logMessages.Clear();
 //        ImGui.Checkbox("Errors", ref _showErrorLogMessages);
     }
-
-    private readonly struct LogMessage {
+    
+    private record LogMessage(string Message, LogSeverity LogSeverity) {
         
-        public string Message { get; }
-        public LogSeverity LogSeverity { get; }
-        
-        public LogMessage(string message, LogSeverity logSeverity) {
-            Message = message;
-            LogSeverity = logSeverity;
-        }
+        public readonly string Message = Message;
+        public readonly LogSeverity LogSeverity = LogSeverity;
         
         public void Draw() {
             ImGui.TextColored(ToColor(LogSeverity), Message);
         }
-
+        
         private static Vector4 ToColor(LogSeverity logSeverity) => logSeverity switch {
             LogSeverity.Normal => new Vector4(1, 1, 1, 1),
             LogSeverity.Success => new Vector4(0, 1, 0, 1),
@@ -67,7 +62,7 @@ public class ConsoleWindow : EditorWindow {
             LogSeverity.Error => new Vector4(1, 0, 0, 1),
             _ => throw new NotImplementedException("log level not implemented")
         };
-
+        
     }
     
 }

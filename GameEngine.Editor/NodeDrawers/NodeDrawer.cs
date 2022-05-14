5844284765dc13f8b3cb90613b63236607df0d49
Conor <conor.huismann@gmail.com>
2022-05-15 01:47:26 +0200

    added PropertyDrawer class for drawing property fields of certain types

--- GameEngine.Editor/NodeDrawers/NodeDrawer.cs
+++ GameEngine.Editor/NodeDrawers/NodeDrawer.cs
@@ -3,6 +3,7 @@ using GameEngine.Core.Nodes;
 using GameEngine.Core.Numerics;
 using GameEngine.Core.Rendering;
 using GameEngine.Core.Serialization;
+using GameEngine.Editor.PropertyDrawers;
 using ImGuiNET;
 
 namespace GameEngine.Editor.NodeDrawers;
@@ -30,7 +31,7 @@ public abstract class NodeDrawer {
         foreach(Type type in derivedTypes) {
             NodeDrawer nodeDrawer = Activator.CreateInstance(type) as NodeDrawer ?? throw new NullReferenceException();
             if(!nodeDrawerLookup.TryAdd(nodeDrawer.NodeType, nodeDrawer))
-                Console.LogWarning($"Failed to register Node Drawer of type {nodeDrawer.NodeType}");
+                Console.LogWarning($"Failed to register Node Drawer for {nodeDrawer.NodeType}");
         }
         return nodeDrawerLookup;
     }
@@ -42,93 +43,17 @@ public abstract class NodeDrawer {
     }
     
     protected static void DrawDefaultDrawers(Node node) {
-        foreach(MemberInfo memberInfo in GetSerializedMembers(node.GetType())) {
+        foreach(MemberInfo memberInfo in GetSerializedMembersNotBeingHidden(node.GetType())) {
             if(memberInfo is PropertyInfo propertyInfo) {
-                DrawProperty(node, propertyInfo);
+                PropertyDrawer.Draw(node, propertyInfo);
             } else if(memberInfo is FieldInfo fieldInfo) {
-                DrawField(node, fieldInfo);
+                PropertyDrawer.Draw(node, fieldInfo);
             }
         }
     }
     
-    private static void DrawField(Node node, FieldInfo fieldInfo) {
-        if(fieldInfo.FieldType == typeof(float)) {
-            float value = (float) fieldInfo.GetValue(node);
-            ImGui.DragFloat(fieldInfo.Name, ref value, 0.01f, float.MinValue, float.MaxValue, "%g");
-            fieldInfo.SetValue(node, value);
-            return;
-        }
-        if(fieldInfo.FieldType == typeof(string)) {
-            string value = (string) fieldInfo.GetValue(node);
-            ImGui.InputText(fieldInfo.Name, ref value, 30);
-            fieldInfo.SetValue(node, value);
-            return;
-        }
-        if(fieldInfo.FieldType == typeof(Color)) {
-            Color value = (Color) fieldInfo.GetValue(node);
-            System.Numerics.Vector4 v4 = new System.Numerics.Vector4(value.R, value.G, value.B, value.A);
-            ImGui.ColorEdit4(fieldInfo.Name, ref v4);
-            value = new Color(v4.X, v4.Y, v4.Z, v4.W);
-            fieldInfo.SetValue(node, value);
-            return;
-        }
-        if(fieldInfo.FieldType == typeof(Vector3)) {
-            Vector3 value = (Vector3) fieldInfo.GetValue(node);
-            System.Numerics.Vector3 v3 = new System.Numerics.Vector3(value.X, value.Y, value.Z);
-            ImGui.DragFloat3(fieldInfo.Name, ref v3, 0.01f);
-            value = new Vector3(v3.X, v3.Y, v3.Z);
-            fieldInfo.SetValue(node, value);
-            return;
-        }
-    }
-    
-    private static void DrawProperty(Node node, PropertyInfo propertyInfo) {
-        if(!propertyInfo.CanRead)
-            return;
-        
-        bool isReadonly = !propertyInfo.CanWrite;
-        MethodInfo? getter = propertyInfo.GetGetMethod(true);
-        MethodInfo? setter = propertyInfo.GetSetMethod(true);
-            
-        if(getter.ReturnType == typeof(float)) {
-            float value = (float) propertyInfo.GetValue(node);
-            ImGui.DragFloat(propertyInfo.Name, ref value, 0.01f, float.MinValue, float.MaxValue, "%g", isReadonly ? ImGuiSliderFlags.NoInput : ImGuiSliderFlags.None);
-            if(!isReadonly)
-                propertyInfo.SetValue(node, value);
-            return;
-        }
-        if(getter.ReturnType == typeof(string)) {
-            string value = (string) propertyInfo.GetValue(node);
-            ImGui.InputText(propertyInfo.Name, ref value, 30);
-            if(!isReadonly)
-                propertyInfo.SetValue(node, value);
-            return;
-        }
-        if(getter.ReturnType == typeof(Color)) {
-            Color value = (Color) propertyInfo.GetValue(node);
-            System.Numerics.Vector4 v4 = new System.Numerics.Vector4(value.R, value.G, value.B, value.A);
-            ImGui.ColorEdit4(propertyInfo.Name, ref v4);
-
-            if(!isReadonly) {
-                value = new Color(v4.X, v4.Y, v4.Z, v4.W);
-                propertyInfo.SetValue(node, value);
-            }
-            return;
-        }
-        if(getter.ReturnType == typeof(Vector3)) {
-            Vector3 value = (Vector3) propertyInfo.GetValue(node);
-            System.Numerics.Vector3 v3 = new System.Numerics.Vector3(value.X, value.Y, value.Z);
-            ImGui.DragFloat3(propertyInfo.Name, ref v3, 0.01f);
-
-            if(!isReadonly) {
-                value = new Vector3(v3.X, v3.Y, v3.Z);
-                propertyInfo.SetValue(node, value);
-            }
-            return;
-        }
-    }
-    
-    private static List<MemberInfo> GetSerializedMembers(Type type) {
+    //todo: exclude if [Serialized(Hidden)]
+    private static List<MemberInfo> GetSerializedMembersNotBeingHidden(Type type) {
         List<MemberInfo> members = new List<MemberInfo>();
         for(Type? currentType = type; currentType is not null; currentType = currentType.BaseType) {
             members.AddRange(
--- GameEngine.Editor/PropertyDrawers/PropertyDrawer.cs
+++ GameEngine.Editor/PropertyDrawers/PropertyDrawer.cs
@@ -1,16 +1,114 @@
+using System.Reflection;
+using GameEngine.Core.Nodes;
+using GameEngine.Core.Numerics;
+using GameEngine.Core.Rendering;
+using GameEngine.Editor.NodeDrawers;
+using ImGuiNET;
+
 namespace GameEngine.Editor.PropertyDrawers;
 
-public abstract class PropertyDrawer<T> {
+
+public abstract class PropertyDrawer {
     
-    internal static Type Type => typeof(T); 
-    public abstract void Draw(T property);
+    private static Dictionary<Type, PropertyDrawer>? _propertyDrawerLookup;
+    protected internal abstract Type PropertyType { get; }
+    protected internal abstract void DrawPropertyInternal(object container, FieldInfo fieldInfo);
+    protected internal abstract void DrawPropertyInternal(object container, PropertyInfo propertyInfo);
+    
+    internal static void Draw(object container, FieldInfo fieldInfo) {
+        _propertyDrawerLookup ??= BuildPropertyDrawerLookup();
+        
+        if(_propertyDrawerLookup.TryGetValue(fieldInfo.FieldType, out PropertyDrawer? propertyDrawer)) {
+            propertyDrawer.DrawPropertyInternal(container, fieldInfo);
+        } else {
+            Console.LogWarning($"Property can't be drawn because there is no property drawer defined for {fieldInfo.FieldType}");
+        }
+    }
+    
+    internal static void Draw(object container, PropertyInfo propertyInfo) {
+        _propertyDrawerLookup ??= BuildPropertyDrawerLookup();
+        
+        if(_propertyDrawerLookup.TryGetValue(propertyInfo.PropertyType, out PropertyDrawer? propertyDrawer)) {
+            propertyDrawer.DrawPropertyInternal(container, propertyInfo);
+        } else {
+            Console.LogWarning($"Property can't be drawn because there is no property drawer defined for {propertyInfo.PropertyType}");
+        }
+    }
+    
+    private static Dictionary<Type, PropertyDrawer> BuildPropertyDrawerLookup() {
+        Dictionary<Type, PropertyDrawer> propertyDrawerLookup = new();
+        List<Type> derivedTypes = ReflectionHelper.GetDerivedTypes(typeof(PropertyDrawer<>), typeof(PropertyDrawer<>).Assembly);
+        foreach(Type type in derivedTypes) {
+            PropertyDrawer propertyDrawer = Activator.CreateInstance(type) as PropertyDrawer ?? throw new NullReferenceException();
+            if(!propertyDrawerLookup.TryAdd(propertyDrawer.PropertyType, propertyDrawer))
+                Console.LogWarning($"Failed to register property drawer for {type.ToString()}");
+        }
+        return propertyDrawerLookup;
+    }
     
 }
 
-public class PropertyDrawerInt : PropertyDrawer<int> {
+public abstract class PropertyDrawer<TProperty> : PropertyDrawer {
     
-    public override void Draw(int property) {
-        
+    protected internal sealed override Type PropertyType => typeof(TProperty);
+    
+    protected internal sealed override void DrawPropertyInternal(object container, FieldInfo fieldInfo) {
+        TProperty value = (TProperty) fieldInfo.GetValue(container);
+        DrawProperty(ref value, new Property(fieldInfo));
+        fieldInfo.SetValue(container, value);
+    }
+    
+    protected internal sealed override void DrawPropertyInternal(object container, PropertyInfo propertyInfo) {
+        TProperty value = (TProperty) propertyInfo.GetValue(container);
+        DrawProperty(ref value, new Property(propertyInfo));
+        if(propertyInfo.CanWrite)
+            propertyInfo.SetValue(container, value);
+    }
+    
+    protected abstract void DrawProperty(ref TProperty value, Property property);
+    
+}
+
+public class PropertyDrawerInt : PropertyDrawer<float> {
+    
+    protected override void DrawProperty(ref float value, Property property) {
+        ImGui.DragFloat(property.Name, ref value, 0.01f, float.MinValue, float.MaxValue, "%g");
+    }
+    
+}
+
+public class PropertyDrawerBool : PropertyDrawer<bool> {
+    
+    protected override void DrawProperty(ref bool value, Property property) {
+        ImGui.Checkbox(property.Name, ref value);
+    }
+    
+}
+
+public class PropertyDrawerColor : PropertyDrawer<Color> {
+    
+    protected override void DrawProperty(ref Color value, Property property) {
+        System.Numerics.Vector4 v4 = new(value.R, value.G, value.B, value.A);
+        ImGui.ColorEdit4(property.Name, ref v4);
+        value = new Color(v4.X, v4.Y, v4.Z, v4.W);
+    }
+    
+}
+
+public class PropertyDrawerString : PropertyDrawer<string> {
+    
+    protected override void DrawProperty(ref string value, Property property) {
+        ImGui.InputText(property.Name, ref value, 30);
+    }
+    
+}
+
+public class PropertyDrawerVector3 : PropertyDrawer<Vector3> {
+    
+    protected override void DrawProperty(ref Vector3 value, Property property) {
+        System.Numerics.Vector3 v3 = new System.Numerics.Vector3(value.X, value.Y, value.Z);
+        ImGui.DragFloat3(property.Name, ref v3, 0.01f);
+        value = new Vector3(v3.X, v3.Y, v3.Z);
     }
     
 }

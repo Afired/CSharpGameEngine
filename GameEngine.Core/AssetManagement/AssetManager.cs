67859f6cb9ebbee1d93ba1cd404844eb9e908ef0
Conor <conor.huismann@gmail.com>
2022-07-22 01:39:07 +0200

    Separation of Core/Editor/Project Assets

--- GameEngine.Core/AssetManagement/AssetManager.cs
+++ GameEngine.Core/AssetManagement/AssetManager.cs
@@ -1,25 +1,32 @@
 using System.IO;
+using System.Linq;
 
 namespace GameEngine.Core.AssetManagement; 
 
 public static class AssetManager {
-
-    public static string[] GetAllTexturePaths() {
-        string folderPath = Path.Join(Configuration.RootPath, Configuration.AssetFolderName, Configuration.TextureFolderName);
-        MakeSureFolderPathExists(folderPath);
-        string[] paths = Directory.GetFiles(folderPath, "*.png", SearchOption.TopDirectoryOnly);
-        return paths;
+    
+    public static string[] GetAllTexturePaths() =>
+        GetAllFilePathsOfAssetsWithExtension("png");
+    
+    public static string[] GetAllShaderPaths() =>
+        GetAllFilePathsOfAssetsWithExtension("glsl");
+    
+    public const string ENGINE_DIRECTORY = @"D:\Dev\C#\CSharpGameEngine";
+    public const string ENGINE_CORE_DIRECTORY = ENGINE_DIRECTORY + @"\GameEngine.Core";
+    public const string ENGINE_CORE_ASSET_DIRECTORY = ENGINE_CORE_DIRECTORY + @"\Assets";
+    public const string ENGINE_EDITOR_DIRECTORY = ENGINE_DIRECTORY + @"\GameEngine.Editor";
+    public const string ENGINE_EDITOR_ASSET_DIRECTORY = ENGINE_EDITOR_DIRECTORY + @"\Assets";
+    
+    public const string PROJECT_DIRECTORY = @"D:\Dev\C#\CSharpGameEngine\ExampleProject";
+    public const string PROJECT_ASSET_DIRECTORY = PROJECT_DIRECTORY + @"\Assets";
+    
+    public static string[] GetAllFilePathsOfAssetsWithExtension(string fileExtension) {
+        // Directory.CreateDirectory(ENGINE_CORE_ASSET_DIRECTORY);
+        // Directory.CreateDirectory(PROJECT_ASSET_DIRECTORY);
+        return 
+            Directory.GetFiles(ENGINE_CORE_ASSET_DIRECTORY, $"*.{fileExtension}", SearchOption.AllDirectories).Concat(
+                Directory.GetFiles(PROJECT_ASSET_DIRECTORY, $"*.{fileExtension}", SearchOption.AllDirectories)
+            ).ToArray();
     }
     
-    public static string[] GetAllShaderPaths() {
-        string folderPath = Path.Join(Configuration.RootPath, Configuration.AssetFolderName, Configuration.ShaderFolderName);
-        MakeSureFolderPathExists(folderPath);
-        string[] paths = Directory.GetFiles(folderPath, "*.glsl", SearchOption.TopDirectoryOnly);
-        return paths;
-    }
-
-    public static void MakeSureFolderPathExists(string folderPath) {
-        Directory.CreateDirectory(folderPath);
-    }
-
 }

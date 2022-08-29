79c786cdbdf3ca5df91a7e9a7c8d169d41da425e
Conor <conor.huismann@gmail.com>
2022-08-29 03:58:06 +0200

    removed null checks for array drawer

--- GameEngine.Editor/PropertyDrawers/PropertyDrawerArray.cs
+++ GameEngine.Editor/PropertyDrawers/PropertyDrawerArray.cs
@@ -4,27 +4,22 @@ namespace GameEngine.Editor.PropertyDrawers;
 
 public class PropertyDrawerArray : PropertyDrawer<Array> {
     
-    protected override void DrawProperty(ref Array? array, Property property) {
+    protected override void DrawProperty(ref Array array, Property property) {
         
-        if(array is null) {
-            ImGui.Text("Array is null");
-        } else {
-            
-            ImGui.Text("Start Array");
-            for(int i = 0; i < array.Length; i++) {
+        ImGui.Text("Start Array");
+        for(int i = 0; i < array.Length; i++) {
 
-                object? element = array.GetValue(i);
+            object? element = array.GetValue(i);
 
-                if(element is null) {
-                    ImGui.Text("Element is null");
-                    continue;
-                }
-                
-                array.SetValue(PropertyDrawer.DrawDirect(element.GetType(), element, property), i);
+            if(element is null) {
+                ImGui.Text("Element is null");
+                continue;
             }
-            ImGui.Text("End Array");
-            
+                
+            array.SetValue(PropertyDrawer.DrawDirect(element.GetType(), element, property), i);
         }
+        ImGui.Text("End Array");
+        
     }
     
 }

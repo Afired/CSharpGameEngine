da1d287595bcc3cd6f12dfb1527ebd419b15d4da
Conor <conor.huismann@gmail.com>
2022-09-22 22:44:05 +0200

    updated bool property drawer

--- GameEngine.Editor/PropertyDrawers/PropertyDrawerBool.cs
+++ GameEngine.Editor/PropertyDrawers/PropertyDrawerBool.cs
@@ -5,7 +5,16 @@ namespace GameEngine.Editor.PropertyDrawers;
 public class PropertyDrawerBool : PropertyDrawer<bool> {
     
     protected override void DrawProperty(ref bool value, Property property) {
-        ImGui.Checkbox(property.Name, ref value);
+        ImGui.Columns(2);
+        ImGui.Text(property.Name);
+        ImGui.NextColumn();
+
+        ImGui.PushItemWidth(ImGui.GetContentRegionAvail().X);
+        ImGui.PushID(property.Name);
+        ImGui.Checkbox("", ref value);
+        ImGui.PopID();
+        ImGui.PopItemWidth();
+        ImGui.Columns(1);
     }
     
 }
